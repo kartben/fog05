1b5f782ebf9f017c16cc81698c831c14f7dbe516
gabrik <gabriele.baldoni@gmail.com>
2017-10-12 16:31:31 +0200

    demo remote controlled

--- src/control.py
+++ src/control.py
@@ -13,12 +13,12 @@ class Controll():
         sid = str(self.uuid)
         self.root = "fos://<sys-id>"
         self.home = str("fos://<sys-id>/%s" % sid)
-        self.nodes = []
+        self.nodes = {}
         # Notice that a node may have multiple caches, but here we are
         # giving the same id to the nodew and the cache
         self.store = DStore(sid, self.root, self.home, 1024)
 
-    def nodeDiscovered(self, uri, value, v):
+    def nodeDiscovered(self, uri, value, v = None):
         value = json.loads(value)
         if uri != str('fos://<sys-id>/%s/' % self.uuid):
             print ("###########################")
@@ -28,30 +28,48 @@ class Controll():
             print ("Name: %s" % value.get('name'))
             print ("###########################")
             print ("###########################")
-            self.nodes.append({value.get('uuid'): value})
+            self.nodes.update({ len(self.nodes)+1 : {value.get('uuid'): value}})
+
+
+
+    def show_nodes(self):
+        for k in self.nodes.keys():
+            n = self.nodes.get(k)
+            id = list(n.keys())[0]
+            print ("%d - %s : %s" % (k, n.get(id).get('name'), id))
+
+
 
     def main(self):
-        # uri = str('fos://<sys-id>/%s/*' % self.uuid)
-        # self.store.observe(uri, self.reactToCache)
+
 
         uri = str('fos://<sys-id>/*/')
         self.store.observe(uri, self.nodeDiscovered)
 
-        input()
 
-        uri = str('fos://<sys-id>/0aa110af-698b-4297-a7d7-9c0ff1ffc72c/plugins')
+
+        while len(self.nodes) == 0:
+            time.sleep(2)
+
+
+        self.show_nodes()
+
+        n = int(input("Select node: "))
+
+        node_uuid = self.nodes.get(n)
+        if node_uuid is not None:
+            node_uuid = list(node_uuid.keys())[0]
+            if node_uuid is not None:
+                pass
+        else:
+            exit()
+
+        uri = str('fos://<sys-id>/%s/plugins' % node_uuid)
         all_plugins = json.loads(self.store.get(uri)).get('plugins')
-        print(all_plugins)
 
         runtimes = [x for x in all_plugins if x.get('type') == 'runtime']
-        print (runtimes)
         print("locating kvm plugin")
         kvm = [x for x in runtimes if 'kvm' in x.get('name')][0]
-        print(kvm)
-        #uri = str('fos://<sys-id>/0aa110af-698b-4297-a7d7-9c0ff1ffc72c/plugins/%s/%s' % (self.uuid, kvm.get('name'),
-        #  kvm.get('uuid')))
-        #kvm = json.loads(self.store.get(uri))
-        print(kvm)
 
 
         vm_uuid = str(uuid.uuid4())
@@ -69,89 +87,48 @@ class Controll():
         print("Press enter to define a vm")
         input()
 
-        uri = str('fos://<sys-id>/0aa110af-698b-4297-a7d7-9c0ff1ffc72c/runtime/%s/entity/%s' % (kvm.get('uuid'), vm_uuid))
+        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s' % (node_uuid, kvm.get('uuid'), vm_uuid))
         self.store.put(uri, json_data)
 
         print("Press enter to configure the vm")
         input()
 
-        uri = str('fos://<sys-id>/0aa110af-698b-4297-a7d7-9c0ff1ffc72c/runtime/%s/entity/%s#status=configure' %
-                  (kvm.get('uuid'), vm_uuid))
+        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s#status=configure' %
+                  (node_uuid, kvm.get('uuid'), vm_uuid))
         self.store.dput(uri)
 
-
-        print("Press enter to configure the vm")
-        input()
-
-        input()
-
-        exit(0)
-
-        while True:
-            time.sleep(100)
-        
-        
-        print("Press enter to define a vm")
-        input()
-        
-        
-        vm_uuid = str(uuid.uuid4())
-        vm_name = 'test'
-        
-        vm_definition = {'name': vm_name, 'uuid': vm_uuid, 'cpu': 1, 'memory': 128, 'disk_size': 5, 'base_image':
-            'http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img' }
-        
-        entity_definition = {'status': 'define', 'name': vm_name, 'version': 1, 'entity_data': vm_definition}
-        
-        json_data = json.dumps(entity_definition)
-        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s' % (self.uuid, kvm.uuid, vm_uuid))
-        self.store.put(uri, json_data)
-        print (self.store)
-        
-        print("Press enter to configure the vm")
-        input()
-        
-        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s#status=configure' % (self.uuid, kvm.uuid, vm_uuid))
-        self.store.dput(uri)
-        print (self.store)
-        
         print("Press enter to start vm")
         input()
-        
-        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s#status=run' % (self.uuid, kvm.uuid, vm_uuid))
+
+        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s#status=run' % (node_uuid, kvm.get('uuid'), vm_uuid))
         self.store.dput(uri)
-        print (self.store)
-        
+
         print("Press enter to stop vm")
         input()
-        
-        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s#status=stop' % (self.uuid, kvm.uuid, vm_uuid))
+
+        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s#status=stop' % (node_uuid, kvm.get('uuid'), vm_uuid))
         self.store.dput(uri)
-        print (self.store)
-        
-        print("Press enter to clean vm")
+
+        print("Press enter to clean the vm")
         input()
-        
-        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s#status=clean' % (self.uuid, kvm.uuid, vm_uuid))
+
+        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s#status=clean' %
+                  (node_uuid, kvm.get('uuid'), vm_uuid))
         self.store.dput(uri)
-        print (self.store)
-        print("Press enter to undefine vm")
+
+        print("Press enter to undefine the vm")
         input()
-        
+
         json_data = json.dumps({'status': 'undefine'})
-        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s' % (self.uuid, kvm.uuid, vm_uuid))
-        self.store.put(uri, json_data)
-        print (self.store)
-        
+        uri = str('fos://<sys-id>/%s/runtime/%s/entity/%s' %
+                  (node_uuid, kvm.get('uuid'), vm_uuid))
+        self.store.dput(uri, json_data)
+
+
+
+        input()
+
         exit(0)
-        
-        
-        
-
-
-
-
-        #################
 
 
 if __name__ == '__main__':
