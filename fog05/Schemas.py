a467a14cb65372bfc22cb3cb2a7b154ae19c8268
gabrik <gabriele.baldoni@gmail.com>
2017-11-17 15:08:59 +0100

    added application definition schema

--- fog05/Schemas.py
+++ fog05/Schemas.py
@@ -10,8 +10,9 @@ def readFile(file_path):
     return data
 
 network_schema = json.loads(readFile(os.path.dirname(__file__)+'/json_objects/network_define.schema'))
-entity_schema = json.loads(readFile(os.path.dirname(__file__)+'/json_objects/entity_define.schema'))
+entity_schema = json.loads(readFile(os.path.dirname(__file__)+'/json_objects/entity_definition.schema'))
 vm_schema = json.loads(readFile(os.path.dirname(__file__)+'/json_objects/vm_define.schema'))
 native_schema = json.loads(readFile(os.path.dirname(__file__)+'/json_objects/native_define.schema'))
 container_schema = json.loads(readFile(os.path.dirname(__file__)+'/json_objects/container_define.schema'))
 ros2_schema = json.loads(readFile(os.path.dirname(__file__)+'/json_objects/ros2_define.schema'))
+app_schema = json.loads(readFile(os.path.dirname(__file__)+'/json_objects/app_definition.schema'))
