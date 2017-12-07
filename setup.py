461d7135cba03df31a230f54ed7e188cefd2eb73
gabrik <gabriele.baldoni@gmail.com>
2017-12-07 15:04:51 +0100

    add shebang

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
+
 from setuptools import setup, find_packages
 
 setup(
@@ -6,7 +7,7 @@ setup(
     version='0.0.2dev',
     author='ATO',
     packages=['fog05', 'fog05/interfaces'],
-    install_requires=['networkx', 'jsonschema', 'click', 'python-daemon'],
+    install_requires=['networkx', 'jsonschema', 'python-daemon'],
     scripts=['bin/fos', 'bin/fos2', 'bin/fos-get'],
     include_package_data=True
 )
