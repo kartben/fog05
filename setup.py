de7437054c4f009088acac9d1c3a1e51db1f2fa9
gabrik <gabriele.baldoni@gmail.com>
2017-12-22 16:03:32 +0100

    some fixes 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fog05',
-    version='0.1.0',
+    version='0.1.1',
     author='ATO',
     packages=['fog05', 'fog05/interfaces'],
     install_requires=['networkx', 'jsonschema', 'python-daemon'],
