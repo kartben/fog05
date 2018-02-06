b1638d8ce52fcff7a96572138436b9b44ab1e5d0
gabrik <gabriele.baldoni@gmail.com>
2018-02-06 13:52:30 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='fog05',
     version='0.1.1',
+    python_requires='>=3',
     author='ATO',
     packages=['fog05', 'fog05/interfaces','fog05/svcs'],
     install_requires=['networkx', 'jsonschema', 'python-daemon','websockets'],
