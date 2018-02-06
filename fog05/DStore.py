27bbe9359dccff220aeaec9a7d642ea28c30026a
Angelo Corsaro <angelo@icorsaro.net>
2018-02-06 16:26:27 +0100

    added resolve to dstore

--- fog05/DStore.py
+++ fog05/DStore.py
@@ -334,8 +334,17 @@ class DStore(Store):
 
         self.notify_observers(uri, None, None)
 
-    def get(self, uri):
+    def resolve(self, uri):
+        rv = self.__controller.resolve(uri)
+        if rv != None:
+            self.logger.debug('DStore', 'URI: {0} was resolved to val = {1} and ver = {2}'.format(uri, rv[0], rv[1]))
+            self.update_value(uri, rv[0], rv[1])
+            self.notify_observers(uri, rv[0], rv[1])
+            return rv[0]
+        else:
+            return None
 
+    def get(self, uri):
         u = uri.split('/')[-1]
         if u.endswith('~') and u.startswith('~'):
             if u in self.__metaresources.keys():
@@ -347,14 +356,7 @@ class DStore(Store):
         if v == None:
             self.__controller.onMiss()
             self.logger.debug('DStore','Resolving: {0}'.format(uri))
-            rv = self.__controller.resolve(uri)
-            if rv != None:
-                self.logger.debug('DStore','URI: {0} was resolved to val = {1} and ver = {2}'.format(uri, rv[0], rv[1]))
-                self.update_value(uri, rv[0], rv[1])
-                self.notify_observers(uri, rv[0], rv[1])
-                return rv[0]
-            else:
-                return None
+            return self.resolve(uri)
         else:
             return v[0]
 
