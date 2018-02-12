d6d51c130a9073a4a2d09c157661dc1e54849a99
gabrik <gabriele.baldoni@gmail.com>
2018-02-12 14:29:32 +0100

    update manifest

--- fog05/Schemas.py
+++ fog05/Schemas.py
@@ -2,7 +2,7 @@ import json
 import pkg_resources
 import os
 resource_package = 'fog05'  # Could be any module/package name
-resource_path = '/'.join(('json_objects', 'network_define.schema'))
+resource_path = '/'.join(('json_objects', 'network_information.schema'))
 
 
 def readFile(file_path):
@@ -11,10 +11,10 @@ def readFile(file_path):
     return data
 
 
-network_schema = json.loads(readFile(os.path.join(os.path.dirname(__file__), 'json_objects', 'network_define.schema')))
+network_schema = json.loads(readFile(os.path.join(os.path.dirname(__file__), 'json_objects', 'network_information.schema')))
 entity_schema = json.loads(readFile(os.path.join(os.path.dirname(__file__), 'json_objects', 'entity_definition.schema')))
 vm_schema = json.loads(readFile(os.path.join(os.path.dirname(__file__), 'json_objects', 'vm.schema')))
 native_schema = json.loads(readFile(os.path.join(os.path.dirname(__file__), 'json_objects', 'native_define.schema')))
-container_schema = json.loads(readFile(os.path.join(os.path.dirname(__file__), 'json_objects', 'container_define.schema')))
+container_schema = json.loads(readFile(os.path.join(os.path.dirname(__file__), 'json_objects', 'container.schema')))
 ros2_schema = json.loads(readFile(os.path.join(os.path.dirname(__file__), 'json_objects', 'ros2_define.schema')))
 app_schema = json.loads(readFile(os.path.join(os.path.dirname(__file__), 'json_objects', 'app_definition.schema')))
