7c7466f57aa232f058af4136d1b4178f00eb3635
gabrik <gabriele.baldoni@gmail.com>
2018-02-18 13:18:34 +0100

    test on json syntax format for value

--- tools/webui/js/app.js
+++ tools/webui/js/app.js
@@ -103,7 +103,7 @@ function refreshtree()
                 console.log(`Value ${v.value}`)
                 console.log(`V is ${v.show()}`)
                 $('#nameNodeTree').html(k);
-                $('#valueNodeTree').html(v.value);
+                $('#valueNodeTree').innerHTML = syntaxHighlight(v.value);
                 $('#keyForm').val(k);
                 $('#valueForm').val(v.value);
             });
@@ -251,5 +251,24 @@ var select = function getSelect() {
     return selects.options[selects.selectedIndex].innerHTML;
 }
 
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
 
 
