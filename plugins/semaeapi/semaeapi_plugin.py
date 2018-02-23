17d47dfffadf3e8a73111946bf3c032ad5ff94be
gabrik <gabriele.baldoni@gmail.com>
2018-02-23 16:08:21 +0100

    update eapi plugin

--- plugins/semaeapi/semaeapi_plugin.py
+++ plugins/semaeapi/semaeapi_plugin.py
@@ -97,7 +97,7 @@ class semaeapi(MonitoringPlugin):
         uri = '{}/{}/{}'.format(self.agent.ahome, self.HOME, uri)
         value = json.dumps(value)
         self.agent.astore.put(uri, value)
-        self.agent.logger("SEMA PLUGIN", "Write to store {} -> {}".format(uri,value))
+        self.agent.logger.info("SEMA PLUGIN", "Write to store {} -> {}".format(uri,value))
 
     def __monitoring_thread(self):
         while self.__monitoring_active:
