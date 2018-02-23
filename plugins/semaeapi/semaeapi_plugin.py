d776bc169688f95964d479368cb3dbecbaedde62
gabrik <gabriele.baldoni@gmail.com>
2018-02-23 08:57:09 +0100

    updated eapi plugin

--- plugins/semaeapi/semaeapi_plugin.py
+++ plugins/semaeapi/semaeapi_plugin.py
@@ -28,7 +28,7 @@ class semaeapi(MonitoringPlugin):
         self.available_api = {}
         self.__updating_thread = None
         self.__monitoring_active = False
-        self.frequency = 30
+        self.frequency = 15
 
         self.start_monitoring()
 
