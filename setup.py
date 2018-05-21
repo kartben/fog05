9252c308a668877708a83f4803ccea071de4362e
gabrik <gabriele.baldoni@gmail.com>
2018-05-21 12:13:58 +0200

    other backportings

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     python_requires='>=3',
     author='ATO',
     packages=['fog05', 'fog05/interfaces','fog05/svcs'],
-    install_requires=['networkx', 'jsonschema', 'python-daemon','websockets'],
+    install_requires=['networkx', 'jsonschema','websockets','psutil','netifaces'],
     #install_requires=['networkx', 'jsonschema', 'python-daemon', 'pydds'],
     scripts=['bin/fos', 'bin/fos-get','bin/fos.bat', 'bin/fos-get.bat','bin/f05ws','bin/f05log.bat',
              'bin/f05log','bin/f05ws.bat','bin/f05wc','bin/f05wc.bat'],
